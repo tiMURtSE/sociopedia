acee3baca23b82b8cdc47f216ef0fa91dfe4b4b1
tiMURtSE <multibhop@mail.ru>
2023-03-27 12:33:27 +0300

    theme config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './index.css';
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import ProfilePage from 'scenes/profilePage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const mode = useSelector((state) => state.mode);
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Routes>
+                        <Route path='/' element={<LoginPage />} />
+                        <Route path='/home' element={<HomePage />} />
+                        <Route path='/profile/:userId' element={<ProfilePage />} />
+                    </Routes>
+                </ThemeProvider>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/scenes/homePage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from 'scenes/navbar';
+
+const HomePage = () => {
+    return (
+        <div>
+            <Navbar />
+            home page
+        </div>
+    );
+};
+
+export default HomePage;
