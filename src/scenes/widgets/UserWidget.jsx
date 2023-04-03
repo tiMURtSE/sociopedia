d5e44bfec5a7eb4a3db5401372a75c6ea1dce966
tiMURtSE <multibhop@mail.ru>
2023-04-03 17:56:16 +0300

    сделан user widget

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -1,4 +1,4 @@
-import { Box, IconButton, Typography, useTheme } from '@mui/material';
+import { Box, Divider, IconButton, Typography, useTheme } from '@mui/material';
 import {
     ManageAccountsOutlined,
     EditOutlined,
@@ -84,7 +84,68 @@ const UserWidget = ({ userId, picturePath }) => {
                 <ManageAccountsOutlined />
             </FlexBetween>
 
+            <Divider />
 
+            <Box p={'1rem 0'}>
+                <Box display='flex' alignItems='center' gap={'1rem'} mb={'0.5rem'}>
+                    <LocationOnOutlined fontSize='large' sx={{color: main}} />
+
+                    <Typography color={medium}>{location}</Typography>
+                </Box>
+
+                <Box display='flex' alignItems='center' gap={'1rem'}>
+                    <WorkOutlineOutlined fontSize='large' sx={{color: main}} />
+
+                    <Typography color={medium}>{occupation}</Typography>
+                </Box>
+            </Box>
+
+            <Divider />
+
+            <Box p='1rem 0'>
+                <FlexBetween mb='0.5rem'>
+                    <Typography color={medium}>Who's viewed your profile</Typography>
+
+                    <Typography color={main} fontWeight='500'>{viewedProfile}</Typography>
+                </FlexBetween>
+                <FlexBetween>
+                    <Typography color={medium}>Impressions of your post</Typography>
+
+                    <Typography color={main} fontWeight='500'>{impressions}</Typography>
+                </FlexBetween>
+            </Box>
+
+            <Divider />
+
+            <Box p='1rem 0'>
+                <Typography fontSize='1rem' fontWeight='500' color={main} mb='1rem'>Social Profiles</Typography>
+
+                <FlexBetween gap='1rem' mb='0.5rem'>
+                    <FlexBetween gap='1rem'>
+                        <img src="../assets/twitter.png" alt="twitter logo" />
+
+                        <Box>
+                            <Typography color={main} fontWeight='500'>Twitter</Typography>
+                            <Typography color={medium}>Social Network</Typography>
+                        </Box>
+                    </FlexBetween>
+
+                    <EditOutlined sx={{ color: main }} />
+                </FlexBetween>
+
+                <FlexBetween gap="1rem">
+                    <FlexBetween gap="1rem">
+                        <img src="../assets/linkedin.png" alt="linkedin" />
+                        
+                        <Box>
+                            <Typography color={main} fontWeight="500">Linkedin</Typography>
+                            <Typography color={medium}>Network Platform</Typography>
+                        </Box>
+                    </FlexBetween>
+
+                    <EditOutlined sx={{ color: main }} />
+                </FlexBetween>
+            </Box>
         </WidgetWrapper>
     );
 };
