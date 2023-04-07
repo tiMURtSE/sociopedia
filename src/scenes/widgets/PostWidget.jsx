fb58452bfee3765731812aa9d0b3925340d46757
tiMURtSE <multibhop@mail.ru>
2023-04-07 13:52:44 +0300

    сделан лайк

--- src/scenes/widgets/PostWidget.jsx
+++ src/scenes/widgets/PostWidget.jsx
@@ -1,10 +1,108 @@
-import React from 'react';
+import {
+    ChatBubbleOutlineOutlined,
+    FavoriteBorderOutlined,
+    FavoriteOutlined,
+    ShareOutlined,
+} from "@mui/icons-material";
+import { Box, IconButton, Typography } from '@mui/material';
+import FlexBetween from 'components/FlexBetween';
+import UserImage from "components/UserImage";
+import WidgetWrapper from 'components/WidgetWrapper';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "state";
 
 const PostWidget = ({ post }) => {
+    const dispatch = useDispatch();
+    const token = useSelector((state) => state.token);
+    const {
+        _id,
+        userId,
+        firstName,
+        lastName,
+        picturePath,
+        description,
+        location,
+        userPicturePath,
+        comments,
+        likes,
+    } = post;
+    const [likesState, setLikesState] = useState(likes);
+
+    const likePost = async () => {
+        const postId = _id;
+        const url = `http://localhost:3001/posts/${postId}/like`;
+        console.log({userId})
+        const response = await fetch(url, {
+            method: 'PATCH',
+            headers: { 
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ userId: userId }),
+        });
+
+        const updatedPost = await response.json();
+
+        dispatch(setPost({ post: updatedPost }));
+        console.log(updatedPost)
+        setLikesState(updatedPost.likes);
+    };
+
     return (
-        <div>
-            {post.description}
-        </div>
+        <WidgetWrapper>
+            {/* PFP, NAME, ADD FRIEND BUTTON */}
+            <FlexBetween>
+                <FlexBetween
+                    gap='1rem'
+                >
+                    <UserImage image={userPicturePath}/>
+
+                    <Box>
+                        <Typography
+                            fontWeight='500'
+                        >
+                            {firstName} {lastName}
+                        </Typography>
+
+                        <Typography>{location}</Typography>
+                    </Box>
+                </FlexBetween>
+
+
+            </FlexBetween>
+
+            {/* DESCRIPTION */}
+            <Typography>{description}</Typography>
+
+            <image width='100%' height='auto' src={`http://localhost:3001/assets/${picturePath}`} />
+
+            {/* LIKES, COMMENTS, SHARE ROW */}
+            <FlexBetween>
+                <FlexBetween>
+                    <FlexBetween>
+                        <IconButton
+                            onClick={likePost}
+                        >
+                            <FavoriteOutlined />
+                        </IconButton>
+
+                        <Typography>{Object.keys(likesState).length}</Typography>
+                    </FlexBetween>
+                    <FlexBetween>
+                        <IconButton>
+                            <ChatBubbleOutlineOutlined />
+                        </IconButton>
+
+                        <Typography>{comments.length}</Typography>
+                    </FlexBetween>
+                </FlexBetween>
+
+                <IconButton>
+                    <ShareOutlined />
+                </IconButton>
+            </FlexBetween>
+        </WidgetWrapper>
     );
 };
 
--- src/scenes/widgets/PostsWidget.jsx
+++ src/scenes/widgets/PostsWidget.jsx
@@ -2,17 +2,14 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setPosts } from 'state';
 import PostWidget from './PostWidget';
-import { Box } from '@mui/material';
 
 const PostsWidget = ({ userId, isProfile = false }) => {
     const posts = useSelector((state) => state.posts);
     const token = useSelector((state) => state.token);
     const dispatch = useDispatch();
-    console.log(posts)
+
     const getPosts = async () => {
-        const url = isProfile 
-            ? `http://localhost:3001/posts/${userId}/posts`
-            : 'http://localhost:3001/posts/';
+        const url = 'http://localhost:3001/posts';
 
         const response = await fetch(url, {
             method: 'GET',
@@ -24,20 +21,33 @@ const PostsWidget = ({ userId, isProfile = false }) => {
         dispatch(setPosts({ posts }));
     };
 
+    const getUserPosts = async () => {
+        const url = `http://localhost:3001/posts/${userId}`;
+
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: { Authorization: `Bearer ${token}` },
+        });
+
+        const posts = await response.json();
+
+        dispatch(setPosts({ posts }));
+    };
+
     useEffect(() => {
-        getPosts();
+        if (isProfile) {
+            getUserPosts();
+        } else {
+            getPosts();
+        }
     }, []);
 
     return (
-        <Box
-            display='flex'
-            flexDirection='column'
-            gap='2rem'
-        >   
-            {posts.map(post => 
-                <PostWidget post={post} key={post._id}/>
+        <>
+            {posts.map(post =>
+                <PostWidget post={post} key={post._id}/>    
             )}
-        </Box>
+        </>
     );
 };
 
